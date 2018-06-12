5f5f204892fe2a8c4f97928a3a6af898ae157e61
Piet Cordemans <piet.cordemans@kuleuven.be>
2018-06-12 16:36:31 +0200

    adc with interrupts example

--- H5/adc/main.cpp
+++ H5/adc/main.cpp
@@ -1,13 +1,46 @@
-#include "adc.h"
 #include "mbed.h"
+
 Serial pc(USBTX, USBRX);
-ADC adc;
+
+uint16_t value = 0xffff;
+
+extern "C"{
+  void ADC_IRQHandler(){
+      value = (LPC_ADC->ADDR5 & 0xFFF0) >> 4; //read from adc channel 5
+      NVIC->ICER[0] |= (1 << 22); //disable ADC interrupt
+  }
+}
+
+void approx_wait(uint32_t milliseconds) {
+	for (uint32_t j = 0; j < milliseconds; j++) {
+		for (volatile uint32_t i = 0; i < 7320; i++);
+	}
+}
+
+void adc_init(){
+  LPC_SC->PCONP |= (1 << 12); //power adc: set the PCADC bit
+  LPC_ADC->ADCR |= (1 << 21); //enable adc: set the PDN bit
+  LPC_ADC->ADCR |= (1 << 8); //PCLK = 24 MHz, CLKDIV = 1+1 = 2 -> ADC clk = 24/2 = 12MHz < 13MHz
+  LPC_PINCON->PINSEL3 |= (3 << 30); //Configure pin AD0.5
+  LPC_ADC->ADCR |= (1 << 5); //Select AD0.5
+  LPC_ADC->ADINTEN &= ~(1 << 8); //Only the individual ADC channels generate interrupts
+  LPC_ADC->ADINTEN |= (1 << 5); //interrupt enabled on channel 5
+}
+
+void adc_start(){
+  NVIC->ISER[0] |= (1 << 22); //enable ADC interrupt
+  LPC_ADC->ADCR |= (1 << 24); //start conversion now
+  approx_wait(1); //needs to wait, not sure why?
+}
 
 int main(){
 
-  adc.start();
+  adc_init();
 
-  pc.printf("%x\r\n", adc.read());
+  adc_start();
+
+  while(value == 0xffff){}
+  pc.printf("%x\r\n",  value);
 
   while(true){
   }
